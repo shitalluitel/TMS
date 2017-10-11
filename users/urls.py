83afe9e09bb3a92cf4acd1855d7e0f55f627573e
ShitalLuitel <ctalluitel@gmail.com>
2017-10-11 15:06:40 +0545

    some change on user app

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,15 @@
-from django.conf.urls import url, include
-
+from django.conf.urls import url
 from . import views
 
 app_name = "users"
 
 urlpatterns = [
-    url(r'^login/$', views.user_login, name='login'),
-    url(r'^register/$', views.user_register, name='register'),
-    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^login/$', views.login_user, name='login_user'),
+    url(r'^logout/$', views.logout_user, name='logout_user'),
+    url(r'^register/$', views.user_register, name='user_register'),
     url(r'^confirm_email/$', views.user_email_confirm, name='email_confirm'),
-    url(r'^change_password/$', views.user_password_change, name='password_change'),
     url(r'^send_password_reset_email/$', views.user_send_password_reset_email, name='send_password_reset_email'),
     url(r'^password_reset/$', views.user_password_reset, name='password_reset'),
-    url(r'^profile/$', views.user_profile_edit, name='profile_edit'),
+    url(r'^change_password/$', views.user_password_change, name='change_password'),
+    url(r'^profile/$', views.user_profile_edit, name="profile_edit"),
 ]
